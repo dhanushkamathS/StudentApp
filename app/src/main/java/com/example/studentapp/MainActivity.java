a56dbe54fc804beed172ff1f51a7e24f383bcf5c
Udit Pathak <pathakprintudit@gmail.com>
2020-06-17 19:22:03 +0530

    This is initial commit by udit

--- app/src/main/java/com/example/studentapp/MainActivity.java
+++ app/src/main/java/com/example/studentapp/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // this is the initial from my side
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
